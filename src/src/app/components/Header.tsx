18d6883ee4de2692b379302e6c5a8662d6ce35e8
areldai03 <dai.0127@icloud.com>
2024-03-22 15:03:26 +0900

    ヘッダーコンポーネント化

---
+++ src/src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header className="bg-black py-5">
+            <div className="container flex justify-between mx-auto text-white">
+                <h1>cafe</h1>
+                <ul className="flex gap-2">
+                    <li>about</li>
+                    <li>abc</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+export default Header
