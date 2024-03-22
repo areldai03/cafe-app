c1aa197f1f3a60cb41e4ecac868ea6cc3912243a
areldai03 <dai.0127@icloud.com>
2024-03-22 15:03:14 +0900

    フッターコンポーネント化

---
+++ src/src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () =>{
+    return(
+        <footer className="absolute bottom-0 p-3">
+            <div>
+                <h1 className="text-2xl">delCafe</h1>
+                <h2>© copyright 2024 md Tel:000-0000-0000</h2>
+            </div>
+        </footer>
+    )
+}
+export default Footer
