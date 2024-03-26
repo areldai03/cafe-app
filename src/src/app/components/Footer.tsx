bb8ee67a5e36edcaa4540904bf0d2a005a3ab905
areldai03 <dai.0127@icloud.com>
2024-03-26 16:52:02 +0900

    フッター固定

--- src/src/app/components/Footer.tsx
+++ src/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () =>{
     return(
-        <footer className="absolute bottom-0 p-3">
+        <footer className="bg-white sticky bottom-0 p-3">
             <div>
                 <h1 className="text-2xl">delCafe</h1>
                 <h2>© copyright 2024 md Tel:000-0000-0000</h2>
