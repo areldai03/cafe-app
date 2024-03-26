b73bbcd3bc6b33f5878addf8010f092d17545723
areldai03 <dai.0127@icloud.com>
2024-03-26 17:04:40 +0900

    Main作成

---
+++ src/src/app/components/Main.tsx
@@ -0,0 +1,14 @@
+const Main = ()=>{
+    return(
+        <main>
+            <div className="container">
+                <div className="flex mt-7">
+                    <img src="pour-cofee.jpg" className="w-40 md:w-auto"/>
+                    <p>coffeeうまい</p>
+                </div>
+
+            </div>
+        </main>
+    )
+}
+export default Main
