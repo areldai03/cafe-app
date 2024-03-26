e9b35b4805c0fdc7efb8169beb85313544a4a041
areldai03 <dai.0127@icloud.com>
2024-03-26 17:04:05 +0900

    Main追加

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Slider from "./components/Slider";
+import Main from "./components/Main"
 
 export default function Home() {
   return (
     <>
       <Slider />
+      <Main />
     </>
   );
 }
