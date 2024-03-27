160684ce00d275d8fbee83082dff4bd159692901
areldai03 <dai.0127@icloud.com>
2024-03-27 17:30:17 +0900

    画像名の修正

--- src/src/app/components/Slider.tsx
+++ src/src/app/components/Slider.tsx
@@ -35,7 +35,7 @@ const Slider = ()=>{
         className="mySwiper"
       >
         <SwiperSlide><img src="cafe.jpg" alt="cafe" /></SwiperSlide>
-      <SwiperSlide><img src="cofee-beans.jpg" alt="cofee-beans" /></SwiperSlide>
+      <SwiperSlide><img src="coffee-cup.jpg" alt="cofee-beans" /></SwiperSlide>
       <SwiperSlide><img src="coffee.jpg" alt="coffee" /></SwiperSlide>
       <SwiperSlide><img src="latte-art.jpg" alt="latte-art" /></SwiperSlide>
       </Swiper>
