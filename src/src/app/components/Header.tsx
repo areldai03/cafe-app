71056deaa23ee7c239ab65c792bd9b0065c1a479
areldai03 <dai.0127@icloud.com>
2024-03-27 17:29:53 +0900

    フォントと画像の追加

--- src/src/app/components/Header.tsx
+++ src/src/app/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className="bg-black py-5">
             <div className="container flex justify-between mx-auto text-white">
-                <h1>cafe</h1>
+                <h1 className="flex font-serif text-xl"><img src="coffee-icon.png" alt="coffee-icon" className="w-10" />Del cafe</h1>
                 <ul className="flex gap-2">
                     <li>about</li>
                     <li>abc</li>
