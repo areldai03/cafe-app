4b658ac415fbb1a18149dd4ee6e0a38125239145
areldai03 <dai.0127@icloud.com>
2024-03-27 17:29:42 +0900

    フォントと画像の追加

--- src/src/app/components/Footer.tsx
+++ src/src/app/components/Footer.tsx
@@ -1,8 +1,11 @@
-const Footer = () =>{
-    return(
+const Footer = () => {
+    return (
         <footer className="bg-white sticky bottom-0 p-3">
-            <div>
-                <h1 className="text-2xl">delCafe</h1>
+            <div className="mx-10">
+                <div className="flex">
+                    <h1 className="text-2xl font-serif">DelCafe</h1>
+                    <img src="coffee-icon.png" alt="coffee-icon" className="w-10" />
+                </div>
                 <h2>© copyright 2024 md Tel:000-0000-0000</h2>
             </div>
         </footer>
