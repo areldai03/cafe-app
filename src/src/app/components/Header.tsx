57be7a0ed403299aca040d4a7f27dd178c91e5f4
areldai03 <dai.0127@icloud.com>
2024-03-29 16:07:08 +0900

    Link実装

--- src/src/app/components/Header.tsx
+++ src/src/app/components/Header.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link"
+
 const Header = () => {
     return (
-        <header className="bg-black py-5">
+        <header className="bg-black p-5">
             <div className="container flex justify-between mx-auto text-white">
-                <h1 className="flex font-serif text-xl"><img src="coffee-icon.png" alt="coffee-icon" className="w-10" />Del cafe</h1>
-                <ul className="flex gap-2">
-                    <li>about</li>
-                    <li>abc</li>
+                <h1 className="flex font-serif text-2xl"><img src="coffee-icon.png" alt="coffee-icon" className="w-10" />Del cafe</h1>
+                <ul className="flex gap-5 text-base md:text-xl">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/menu">Menu</Link></li>
+                    <li>求人情報</li>
                 </ul>
             </div>
         </header>
