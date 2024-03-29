1624fd85b39651403c6c2639a301718d5990be24
areldai03 <dai.0127@icloud.com>
2024-03-29 16:06:38 +0900

    メニューの追加

---
+++ src/src/app/(pages)/menu/page.tsx
@@ -0,0 +1,18 @@
+const Menu = () => {
+    return (
+        <>
+            <div className="relative">
+                <img src="menu.jpg" className="object-contain w-full brightness-50 bg-black/30 md:h-96" alt="Menu" />
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <span className="text-white text-5xl md:text-9xl">Menu</span>
+                </div>
+            </div>
+
+            <div className="container">menu</div>
+        </>
+
+
+    );
+};
+
+export default Menu;
