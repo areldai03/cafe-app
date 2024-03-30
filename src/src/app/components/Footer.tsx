752f2fb19a1b8b96764bd15ae9ceb70915a9322e
areldai03 <dai.0127@icloud.com>
2024-03-30 21:04:01 +0900

    フォントサイズ修正

--- src/src/app/components/Footer.tsx
+++ src/src/app/components/Footer.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
         <footer className="bg-white sticky bottom-0 p-3">
             <div className="mx-10">
                 <div className="flex">
-                    <h1 className="text-2xl font-serif">DelCafe</h1>
+                    <h1 className="text-xl font-serif md:text-2xl">DelCafe</h1>
                     <img src="coffee-icon.png" alt="coffee-icon" className="w-10" />
                 </div>
-                <h2>© copyright 2024 md Tel:000-0000-0000</h2>
+                <h2 className="text-sm md:text-lg">© copyright 2024 md Tel:000-0000-0000</h2>
             </div>
         </footer>
     )
