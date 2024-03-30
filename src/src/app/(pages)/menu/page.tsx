8aa042be3cbb1f8bf3e4c0c52fb8dd754cdbb58a
areldai03 <dai.0127@icloud.com>
2024-03-30 21:04:14 +0900

    メニュー画面作成

--- src/src/app/(pages)/menu/page.tsx
+++ src/src/app/(pages)/menu/page.tsx
@@ -4,11 +4,43 @@ const Menu = () => {
             <div className="relative">
                 <img src="menu.jpg" className="object-contain w-full brightness-50 bg-black/30 md:h-96" alt="Menu" />
                 <div className="absolute inset-0 flex items-center justify-center">
-                    <span className="text-white text-5xl md:text-9xl">Menu</span>
+                    <span className="text-white font-serif text-5xl md:text-9xl">Menu</span>
                 </div>
             </div>
 
-            <div className="container">menu</div>
+            <div className="flex justify-center">
+                <div className="p-5 md:w-2/3">
+                    <h2 className="border-y-2 border-amber-700 font-serif text-xl md:text-3xl">blended coffee</h2>
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">異次元のブレンド............................. ￥500</p>
+                    </div>
+                    
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">アメリカンブレンド.......................... ￥500</p>
+                    </div>
+
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">ソフトブレンド................................. ￥500</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="p-5 md:w-2/3">
+                    <h2 className="border-y-2 border-amber-700 font-serif text-xl md:text-3xl">straight coffee</h2>
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">モカイルガチェフェ............................. ￥500</p>
+                    </div>
+                    
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">サントゥアリオ.................................... ￥500</p>
+                    </div>
+
+                    <div className="p-3">
+                        <p className="text-xs md:text-base">トミオブルボン.................................... ￥500</p>
+                    </div>
+                </div>
+            </div>
         </>
 
 
